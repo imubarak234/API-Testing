595ee682fb5210dd42961b4fe3d9f8ae47a997a7
Mubarak <imubarak2424@gmail.com>
2022-09-07 17:59:12 +0100

    more testing

--- more_test.js
+++ more_test.js
@@ -1,8 +1,28 @@
-5 
+const body = {
+  item_id: "item3",
+  title: "testaaa",
+  category: "Fictleaf",
+}
 
-let mike = 3;
+const datas = { 
+  "item_id": "item1",
+}
 
-console.log(mike = mike + 2)
-console.log(mike = mike * 3)
-console.log(mike = mike / 3)
-console.log(mike = mike - 2)
+fetch("https://api.twilio.com/2010-04-01/Accounts/AC21613e24f57fc71ea268a3f8213b31f6/Messages.json", {
+  method: "GET",
+  mode: "no-cors",
+  credentials: {
+    'System' : 'Management',
+    'Automation' : ''
+  },
+  //Accept: "application/json",
+  //Authorization: "Basic TXViYXJha0licmFoaW0gKHB1YmxpYyk6amw4LWEzVmJsX25scXl6cVJwU3BZVkFKUW9qSmM0OTY=",
+  headers: {
+    "X-Auth-Token": "jl8-a3Vbl_nlqyzqRpSpYVAJQojJc496",
+    "Content-Type": "application/json",
+    Authorization: "Basic TXViYXJha0licmFoaW0gKHB1YmxpYyk6amw4LWEzVmJsX25scXl6cVJwU3BZVkFKUW9qSmM0OTY=",
+  }
+})
+  .then(response => response.json())
+  .then(json => console.log(json))
+
