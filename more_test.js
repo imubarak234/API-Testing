bb97c777400e540f2b65332e50d82a03da407af7
imubarak234 <imubarak2424@gmail.com>
2022-09-22 12:30:53 +0100

    more tests

--- more_test.js
+++ more_test.js
@@ -1,25 +1,9 @@
 
-const sgMail = require('@sendgrid/mail')
+// const sgMail = require('@sendgrid/mail')
 // sgMail.setApiKey('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')
 
-console.log(process.env.SENDGRID_API_KEY)
-// fetch("https://api.twilio.com/2010-04-01/Accounts/AC21613e24f57fc71ea268a3f8213b31f6/Messages.json", {
-//   method: "GET",
-//   mode: "no-cors",
-//   credentials: {
-//     'System' : 'Management',
-//     'Automation' : ''
-//   },
-//   //Accept: "application/json",
-//   //Authorization: "Basic TXViYXJha0licmFoaW0gKHB1YmxpYyk6amw4LWEzVmJsX25scXl6cVJwU3BZVkFKUW9qSmM0OTY=",
-//   headers: {
-//     "X-Auth-Token": "jl8-a3Vbl_nlqyzqRpSpYVAJQojJc496",
-//     "Content-Type": "application/json",
-//     Authorization: "Basic TXViYXJha0licmFoaW0gKHB1YmxpYyk6amw4LWEzVmJsX25scXl6cVJwU3BZVkFKUW9qSmM0OTY=",
-//   }
-// })
-//   .then(response => response.json())
-//   .then(json => console.log(json))
+// console.log(process.env.SENDGRID_API_KEY)
+
 
 
 // const msg = {
@@ -38,3 +22,101 @@ console.log(process.env.SENDGRID_API_KEY)
 //   .catch((error) => {
 //     console.error(error)
 //   })
+const firstFetch = () => {
+  fetch("https://api.sendgrid.com/v3/mail/send", {
+  method: "POST",
+  //mode: "no-cors",
+  // credentials: {
+  //   'System' : 'Management',
+  //   'Automation' : ''
+  // },
+  //Accept: "application/json",
+  //Authorization: "Basic TXViYXJha0licmFoaW0gKHB1YmxpYyk6amw4LWEzVmJsX25scXl6cVJwU3BZVkFKUW9qSmM0OTY=",
+  headers: {
+    // "X-Auth-Token": "jl8-a3Vbl_nlqyzqRpSpYVAJQojJc496",
+    "Content-Type": "application/json",
+    'Authorization': "Bearer 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",
+  },
+
+  body: JSON.stringify({
+    'personalizations' : [
+      {
+        'to' : [
+          {
+            'email' : 'imubarak2424@gmail.com'
+          }
+        ]
+      }
+    ],
+    'from': {
+      'email' : 'imubarak2424@gmail.com'
+    },
+    'subject' : 'Testing!!',
+    'content' : [
+      {
+        'type' : 'text/plain',
+        'value' : 'Heya!'
+      }
+    ]
+  }),
+})
+  .then((response) => response.json())
+  .then(json => {console.log(json)})
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+}
+
+
+const secondFetch = () => {
+  let url = 'https://docs.google.com/spreadsheets/d/1kyXZddzslOrQwXKWARsJXTNs7ZYj_DgbKovNdyoriJU/';
+
+  fetch(url)
+  .then(res => res.text())
+  .then(data => {
+    console.log(data)
+  })
+}
+
+const thirdFetch = () => {
+  let url = 'https://sheet.best/api/sheets/150d2564-50dd-4282-9031-ccc30b646aed';
+
+  fetch(url)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+  })
+}
+
+const postToSheat = () => {
+  //let url = 'https://sheet.best/api/sheets/150d2564-50dd-4282-9031-ccc30b646aed'; 
+
+  fetch("https://sheet.best/api/sheets/5f05d562-cf94-492b-aac8-195d3c57ec67", {
+  method: "POST",
+  mode: "cors",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({
+    "Frist" : "Mike",
+    "Last " : "Mike",
+    "ID" : "7",
+    "Value" : "100",
+    "Created at": new Date(),
+  })
+})
+  .then(response => response.json())
+  .then(data => console.log('success: ', data))
+  .catch((error) => {
+    console.error('Error: ', error)
+  })
+}
+
+const buttonFetch = document.getElementById('spreads');
+
+buttonFetch,addEventListener('click', (e) => {
+  postToSheat();
+});
+  
+
+ //  https://docs.google.com/spreadsheets/d/1kyXZddzslOrQwXKWARsJXTNs7ZYj_DgbKovNdyoriJU/edit?usp=sharing
