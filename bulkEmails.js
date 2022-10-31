7de29c6bc339ef0d349630ec7e28cbf8c809c1d3
Mubarak <imubarak2424@gmail.com>
2022-10-31 14:52:26 +0100

    new tests to commit to repo

--- bulkEmails.js
+++ bulkEmails.js
@@ -1,23 +1,107 @@
+const testData = [{
+  Categories : "Participants",
+  "Company/Organization" : null,
+  Date : null,
+  Email : "imubarak2424@gmail.com",
+  "First Name" : "JUNAIDU",
+  "Institutional Affiliation" : null,
+  "Last Name" : "SALISU",
+  "Phone Number" : null
+}, {
+  Categories : "Participants",
+  "Company/Organization" : null,
+  Date : null,
+  Email : "m.bthedon@yahoo.com",
+  "First Name" : "JUNAIDU",
+  "Institutional Affiliation" : null,
+  "Last Name" : "SALISU",
+  "Phone Number" : null
+}, {
+  Categories : "Participants",
+  "Company/Organization" : null,
+  Date : null,
+  Email : "shithappens242@gmail.com",
+  "First Name" : "JUNAIDU",
+  "Institutional Affiliation" : null,
+  "Last Name" : "SALISU",
+  "Phone Number" : null
+}];
 
-const looper = () => {
-  let count = 0;
-  const setting = setInterval(function(){
-    if (count >= 2){
-      console.log("ended")
-      clearInterval(setting)
-    }
-    else {
-      console.log("Time Interval")
-      count ++;
-    }
-  },1000 * 2);
+    //   fetch('http://localhost:3000/api/v1/emails', {
+    //   method: 'POST',
+    //   mode: 'no-cors',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify({
+    //     "email" : testData[0].Email,
+    //     "name" : `${testData[0]["Last Name"]} ${testData[0]["First Name"]}`,
+    //     "category" : testData[0].Categories
+    //   }),
+    // })
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     console.log('success: ', data);
+    //   })
+
+      // async function postData(url = '', data = {}) {
+      //   // Default options are marked with *
+      //   const response = await fetch(url, {
+      //     method: 'POST', 
+      //     mode: 'no-cors', 
+      //     cache: 'no-cache', 
+      //     credentials: 'same-origin', 
+      //     headers: {
+      //       'Content-Type': 'application/json',
+            
+      //     },
+          
+      //     body: JSON.stringify(data) // body data type must match "Content-Type" header
+      //   });
+      //   return response.text();// parses JSON response into native JavaScript objects
+      // }
+      
+      // postData('http://localhost:3000/api/v1/emails', {
+      //   "email" : testData[0].Email,
+      //   "name" : `${testData[0]["Last Name"]} ${testData[0]["First Name"]}`,
+      //   "category" : testData[0].Categories
+      // })
+      // .then((data) => {
+      //   console.log(JSON.parse(data));
+      // });
+
+const looper = async (data) => {
+ 
+  for(let x = 733; x < data.length; x++){
+
+    
+    await fetch('http://localhost:3000/api/v1/emails', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        "email" : data[x].Email,
+        "name" : `${data[x]["Last Name"]} ${data[x]["First Name"]}`,
+        "category" : data[x].Categories
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(x)
+        console.log('success: ', data);
+      })
+      .catch((error) => {
+        console.error('Error: ', error);
+      });
+  }
 };
 
-looper();
-
-fetch('https://sheet.best/api/sheets/5f05d562-cf94-492b-aac8-195d3c57ec67')
-  .then((res) => (res.json()))
-  .then((data) => {
-    console.log(data)
-
-  })
+ //looper(testData);
+// console.log("Its working at this put")
+// fetch('https://sheet.best/api/sheets/7d520258-c5f2-4cc6-8644-436ff5e3e6c5')
+//   .then((res) => (res.json()))
+//   .then((data) => {
+//     console.log(data)
+//     looper(data);
+//   })
