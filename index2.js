e446362699eae2300dd47acd041fabcb17f37530
Mubarak <imubarak2424@gmail.com>
2022-11-15 09:45:56 +0100

    complete website test

--- index2.js
+++ index2.js
@@ -6,11 +6,34 @@ const generatePDF = async (name) => {
     return res.arrayBuffer()
   })
 
+  const exFont = await fetch("./Sanchez-Regular.ttf")
+  .then(res => {
+    return res.arrayBuffer();
+  })
+
   const pdfDoc = await PDFDocument.load(exBytes);
 
+  console.log(pdfDoc);
+
+  pdfDoc.registerFontkit(fontkit)
+
+  const myFont = await pdfDoc.embedFont(exFont);
+
+  const pages = pdfDoc.getPages();
+  const firstPg = pages[0];
+
+  firstPg.drawText(name, {
+    x: 220,
+    y: 300,
+    size: 45,
+    font: myFont,
+    color: rgb(0,0,0)
+  });
+
   const uri = await pdfDoc.saveAsBase64({dataUri: true});
+  window.open(uri);
 
   document.querySelector("#mypdf").src = uri;
 }
 
-generatePDF();
+generatePDF("Mubarak Ibrahim");
